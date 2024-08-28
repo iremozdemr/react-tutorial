75094e563c78afc8bccc37887e8c6326c3529b9f
İrem Özdemir <irem.ozdemir@etu.edu.tr>
2024-08-28 21:45:16 +0300

    adding css to react

---
+++ project5/src/App.jsx
@@ -0,0 +1,14 @@
+import Button from "./Button/Button.jsx"
+
+function App() {
+  return(
+    <Button/>
+  );
+}
+
+export default App
+
+//react'a css ekleme:
+//external
+//internal
+//modules
---
+++ project5/src/Button/Button.jsx
@@ -0,0 +1,9 @@
+import styles from "./Button.module.css"
+
+function Button(){
+    return(
+        <button className={styles.button}>click me</button>
+    );
+}
+
+export default Button
